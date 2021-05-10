3c024486773d2b458611cc91b5f589e75399df5b
PushkarRJain <prj20012016@gmail.com>
2021-05-10 19:55:01 +0530

    Logo Image

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+import Content from './components/home_content'
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Content />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/home_content.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function home_content() {
+  return (
+    <div>
+      <div class="row">
+        <div class='col-lg-12 col-sm-6'>
+          <h1>Banking System</h1>
+        </div>
+        <p>
+          Lorem ipsum
+        </p>
+      </div>
+    </div>
+    
+    );
+  }
+  export default home_content;
