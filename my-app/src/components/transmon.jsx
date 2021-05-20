321d445ff6c5c00564b66f7b1d2e2a428ea7a06a
PushkarRJain <prj20012016@gmail.com>
2021-05-20 18:24:43 +0530

    Transfers Page Implemented with animation effects

--- my-app/src/components/transmon.jsx
+++ my-app/src/components/transmon.jsx
@@ -1,9 +1,104 @@
-import React from "react";
-function transmon() {
+import React, {useState, useEffect} from "react";
+import Axios from "axios";
+import gif from "../images/buff.gif" 
+function Transmon() {
+  const [name_1, setname_1] = useState("");
+  const [name_2,setname_2] = useState("");
+  const [amt,setamt] = useState("");
+  const submitReview = function(e){
+    e.preventDefault();
+   console.log(name_1, name_2, amt);
+   var x = document.getElementById("div1");
+   x.style.display = "none";
+   var y = document.getElementById("div2");
+   y.style.display = "block";
+
+   var link = "http://localhost:3001/transact/"+name_1+"/"+name_2+"/"+amt ;
+   console.log(link);
+   Axios.get(link).then((response)=>{
+     var flag = response.data.flag;
+     console.log(flag);
+     function disapper()
+     {
+       y.style.display = "none";
+    
+     if(flag === 1)
+     {
+      var z = document.getElementById("div3_s");
+      z.style.display = "block";
+     }
+     else
+     {
+      var a = document.getElementById("div3_f");
+      a.style.display = "block";
+     }
+    }
+    setTimeout(disapper, 5000);
+   })
+  }
   return(
+    
     <div>
-      <h1>Transfer Money</h1>
+      
+      <form id="div1">
+      <h1 className="viewh1">Money Transfer</h1>
+      <div class="container">
+      <div class="form-row">
+    <div class="form-group col-md-6">
+    <label >Transferee</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Customer Name" 
+       onChange = {function(e){
+        setname_1(e.target.value);
+        }}
+    />
+    </div>
+    <div class="form-group col-md-6">
+    <label>Receiver</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Customer Name"
+       onChange = {function(e){
+        setname_2(e.target.value);
+        }}
+    />
+    </div>
+  </div>
+
+  <div class="form-group amt">
+    <label for="exampleFormControlInput1">Amount</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="₹"
+       onChange = {function(e){
+        setamt(e.target.value);
+        }}
+    />
+  </div>
+  <button type="submit" class="btn" id="paybut" onClick={submitReview}>Transfer</button>
+</div>
+</form>
+<div id="div2"><img src = {gif}/></div>
+<div id="div3_s">
+<div class="container">
+      <div class="cardpay">
+        <div class="title">
+     
+      <h1 class="h2pay">Transaction successful</h1>
+    </div>
+
+  <div class="circle"></div>
+  </div>
+</div>
+</div>
+<div id="div3_f">
+<div class="container">
+      <div class="cardpayf">
+        <div class="title">
+     
+      <h1 class="h2payf">Transaction Failed</h1>
+    </div>
+
+  <div class="circle"></div>
+  </div>
+</div>
+</div>
     </div>
   );
 }
-export default transmon;
+export default Transmon;
--- server/index.js
+++ server/index.js
@@ -56,6 +56,85 @@ app.get("/custinfo/:inp",function(req,res){
     }
 });
 
+
+
+})
+app.get("/transact/:name/:names/:amt",function(req,res){
+  var cust_1 = req.params.name;
+  var cust_2 = req.params.names;
+  var amount = req.params.amt;
+  global.balance_1 = 0;
+  global.balance_2 = 0;
+  var flag = 0;
+  console.log(cust_1,cust_2,amount);
+  const sqlSearch_1="SELECT * FROM customers WHERE customers.name=(?)";
+  db.query(sqlSearch_1,[cust_1],function(err,response){
+    if(err)
+    {
+      
+        console.log(err);
+    }
+    else
+    {
+      
+        //console.log("Entry saved into DB"); 
+        balance_1 = response[0].balance;
+        const sqlSearch_2="SELECT * FROM customers WHERE customers.name=(?)";
+        db.query(sqlSearch_2,[cust_2],function(err,response){
+          if(err)
+          {
+            
+              console.log(err);
+          }
+          else
+          {
+              balance_2 = response[0].balance;
+              console.log(balance_1,balance_2);
+              console.log(amount);
+              balance_1 = Number(balance_1) - Number(amount);
+              balance_2 = Number(balance_2) + Number(amount);
+              console.log(balance_1,balance_2);
+              if(Number(balance_1)<0)
+              {
+                console.log("insuff");
+                res.send({"flag":flag});
+                
+              }
+              else{
+                flag =1;
+                console.log("success");
+                const sqlUpdate_1 = "UPDATE customers SET balance=(?) WHERE name=(?)"
+                db.query(sqlUpdate_1,[balance_1,cust_1],function(err,reply){
+                  if(err)
+                  {
+                      console.log(err);
+                  }
+                  else
+                  {
+                    console.log(reply);
+                    const sqlUpdate_2 = "UPDATE customers SET balance=(?) WHERE name=(?)"
+                    db.query(sqlUpdate_2,[balance_2,cust_2],function(err,reply){
+                      if(err)
+                      {
+                          console.log(err);
+                      }
+                      else
+                      {
+                        console.log(reply);
+                        res.send({"flag":flag});
+                      }
+                    });
+                  }
+                });
+
+                
+              }
+          }
+        });
+    }
+});
+
+
 })
 app.listen(3001,function(){
   console.log("Port 3001 up and running!");
